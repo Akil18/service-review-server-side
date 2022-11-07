33b9021f63fa12f7be9d8d2a2a89f396931764b2
Akil Chowdhury <akiluddinchy@gmail.com>
2022-11-08 01:33:36 +0600

    connect to mongoDB and set env variables

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require('express');
-
-const cors = require('cors');
-
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
 const app = express();
 const port = 5000 || process.env.PORT;
 
+// middleware
 app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('server is up');
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nvyyp05.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", (req, res) => {
+  res.send("server is up");
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
